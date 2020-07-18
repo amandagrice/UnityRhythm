b7675201a890d5a9a0053b5240c8cfac07001b1a
Amanda Grice <algrice@bu.edu>
2020-07-18 14:16:23 -0400

    add script to control button presses

---
+++ Assets/Scripts/HitZoneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitZoneController : MonoBehaviour {
+
+	public KeyCode key;
+	private Vector3 originalPosition;
+
+    void Start() {
+        originalPosition = this.transform.position;
+    }
+
+    void Update() {
+    	if (Input.GetKey(key)) {
+    		if (this.transform.position.y > 0) {
+            	this.transform.position += Vector3.down * Time.deltaTime;
+    		}
+        } else {
+        	this.transform.position = originalPosition;
+        }
+    }
+}
